40f784265f23158fa5c6282b574a17d92c319d04
Trim Bresilla <trim.bresilla@gmail.com>
2026-02-13 00:11:44 +0100

    feat(mux): implement hexe.mux.config.set() C API

--- src/core/api_bridge.zig
+++ src/core/api_bridge.zig
@@ -4,6 +4,7 @@ const Lua = zlua.Lua;
 const LuaState = zlua.LuaState;
 const config_builder = @import("config_builder.zig");
 const ConfigBuilder = config_builder.ConfigBuilder;
+const config = @import("config.zig");
 
 /// Registry key for storing ConfigBuilder pointer
 const BUILDER_REGISTRY_KEY = "_hexe_config_builder";
@@ -69,4 +70,74 @@ pub fn getPopBuilder(lua: *Lua) !*config_builder.PopConfigBuilder {
     }
 
     return builder.pop.?;
+}
+
+// ===== MUX API Functions =====
+
+/// Lua C function: hexe.mux.config.set(key, value)
+pub export fn hexe_mux_config_set(L: ?*LuaState) callconv(.C) c_int {
+    const lua: *Lua = @ptrCast(L);
+
+    // Get key (arg 1)
+    const key = lua.toString(1) catch {
+        _ = lua.pushString("config.set: key must be a string");
+        lua.raiseError();
+    };
+
+    // Get MuxConfigBuilder
+    const mux = getMuxBuilder(lua) catch {
+        _ = lua.pushString("config.set: failed to get config builder");
+        lua.raiseError();
+    };
+
+    // Type-based value parsing (arg 2)
+    const val_type = lua.typeOf(2);
+
+    // Boolean options
+    if (val_type == .boolean) {
+        const val = lua.toBoolean(2);
+        if (std.mem.eql(u8, key, "confirm_on_exit")) {
+            mux.confirm_on_exit = val;
+        } else if (std.mem.eql(u8, key, "confirm_on_detach")) {
+            mux.confirm_on_detach = val;
+        } else if (std.mem.eql(u8, key, "confirm_on_disown")) {
+            mux.confirm_on_disown = val;
+        } else if (std.mem.eql(u8, key, "confirm_on_close")) {
+            mux.confirm_on_close = val;
+        } else if (std.mem.eql(u8, key, "winpulse_enabled")) {
+            mux.winpulse_enabled = val;
+        } else {
+            const msg = std.fmt.allocPrint(mux.allocator, "config.set: unknown boolean key '{s}'", .{key}) catch "config.set: unknown key";
+            _ = lua.pushString(msg);
+            lua.raiseError();
+        }
+        return 0;
+    }
+
+    // Number options
+    if (val_type == .number) {
+        const val_f64 = lua.toNumber(2) catch {
+            _ = lua.pushString("config.set: failed to parse number");
+            lua.raiseError();
+        };
+
+        if (std.mem.eql(u8, key, "winpulse_duration_ms")) {
+            mux.winpulse_duration_ms = @intFromFloat(val_f64);
+        } else if (std.mem.eql(u8, key, "winpulse_brighten_factor")) {
+            mux.winpulse_brighten_factor = @floatCast(val_f64);
+        } else if (std.mem.eql(u8, key, "selection_color")) {
+            mux.selection_color = @intFromFloat(val_f64);
+        } else if (std.mem.eql(u8, key, "mouse_selection_override_mods")) {
+            mux.mouse_selection_override_mods = @intFromFloat(val_f64);
+        } else {
+            const msg = std.fmt.allocPrint(mux.allocator, "config.set: unknown number key '{s}'", .{key}) catch "config.set: unknown key";
+            _ = lua.pushString(msg);
+            lua.raiseError();
+        }
+        return 0;
+    }
+
+    // Unknown type
+    _ = lua.pushString("config.set: value must be boolean or number");
+    lua.raiseError();
 }
